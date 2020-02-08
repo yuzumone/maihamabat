a2b7dde4b5cf49c4071c4ab686c7e16ae49d0c6a
Kazuaki Togawa <ktogawa@yahoo-corp.jp>
2020-02-08 17:37:20 +0900

    Add version command

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,15 @@
+import 'dart:io';
 import 'package:args/command_runner.dart';
 import 'package:maihamabat/notify_comingsoon.dart';
+import 'package:maihamabat/version.dart';
 
 void main(List<String> arguments) {
   var runner = CommandRunner('maihamabat', 'batch operation for maihama.')
     ..addCommand(NotifyComingsoon())
-    ..run(arguments);
+    ..addCommand(Version())
+    ..run(arguments).catchError((error) {
+      if (error is! UsageException) throw error;
+      print(error);
+      exit(64);
+    });
 }
