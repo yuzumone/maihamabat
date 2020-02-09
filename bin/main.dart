7f863fd356cf0501c7b88fe93d5dbd0e2703fb69
Kazuaki Togawa <ktogawa@yahoo-corp.jp>
2020-02-09 13:34:04 +0900

    Add restaurant reservation

--- bin/main.dart
+++ bin/main.dart
@@ -2,14 +2,17 @@ import 'dart:io';
 import 'package:args/command_runner.dart';
 import 'package:maihamabat/notify_comingsoon.dart';
 import 'package:maihamabat/version.dart';
+import 'package:maihamabat/reservation.dart';
 
 void main(List<String> arguments) {
   var runner = CommandRunner('maihamabat', 'batch operation for maihama.')
     ..addCommand(NotifyComingsoon())
     ..addCommand(Version())
+    ..addCommand(Reservation())
     ..run(arguments).catchError((error) {
       if (error is! UsageException) throw error;
       print(error);
       exit(64);
     });
+  ;
 }
